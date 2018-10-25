353b0d1d517aa82c6fb6144d7a599eaecaf2b14d
Owen Lacey <owen.lacey@audacia.co.uk>
2018-10-25 16:08:54 +0100

    - support configuring what enum values should be ouputted as - default to string output types - updated  example.config to not fall over by default

---
+++ Audacia.Typescript.Transpiler/Configuration/EnumSettings.cs
@@ -0,0 +1,19 @@
+﻿using System.Xml.Serialization;
+
+namespace Audacia.Typescript.Transpiler.Configuration
+{
+    public class EnumSettings
+    {
+
+        [XmlAttribute("valueType")]
+        public EnumValueType ValueType { get; set; }
+    }
+
+
+    public enum EnumValueType
+    {
+        Number,
+        String
+    }
+
+}
