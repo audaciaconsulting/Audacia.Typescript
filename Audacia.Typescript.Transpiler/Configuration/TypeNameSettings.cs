3e97d54a08492c43484f68a48da7a64a256eef4b
Owen Lacey <owen.lacey@audacia.co.uk>
2018-10-26 09:15:27 +0100

    - support specifying type names in config - extension method to filter types based on input config - separated out tests & production code into solution folders

---
+++ Audacia.Typescript.Transpiler/Configuration/TypeNameSettings.cs
@@ -0,0 +1,10 @@
+﻿using System.Xml.Serialization;
+
+namespace Audacia.Typescript.Transpiler.Configuration
+{
+    public class TypeNameSettings
+    {
+        [XmlAttribute("name")]
+        public string Name { get; set; }
+    }
+}
